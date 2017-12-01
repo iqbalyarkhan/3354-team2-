b07cecfde7715db2976450301efe451ef4463fbb
Anmol Agarwal <31830722+aagarwal21@users.noreply.github.com>
2017-12-01 17:37:55 -0600

    got rid of unnecessary imports/ tests

--- src/CalendarApp/app/src/main/java/team2/calendarapp/AgendaView.java
+++ src/CalendarApp/app/src/main/java/team2/calendarapp/AgendaView.java
@@ -1,26 +1,20 @@
 package team2.calendarapp;
 
 import android.arch.lifecycle.Lifecycle;
-import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
 
 public class AgendaView extends Fragment {
 /**
 AgendaView is the view for displaying calendar events as an agenda list.
 The AgendaView view displays events in a list and shows event name, event start time, and event end time
 */
-DateFormat format = new SimpleDateFormat("dd MM year");
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,19 +27,10 @@ DateFormat format = new SimpleDateFormat("dd MM year");
         super.onCreate(savedInstanceState);
         View root = inflater.inflate(R.layout.fragment_agenda,container,false);
         ListView agendaListView = root.findViewById(R.id.agenda_list_view);
-        // creating a test event
-        Date d = new Date(2017,3,5);
-        Cursor e = null;
-        EventDatabase.getInstance(getContext()).addRecord(1,"Test event ","this is a test","loc",1,2, d,"user" );
-
+      
         // getting calendar events list
         Event [] agendaEvents = EventDB.getEvents();
-     //  ArrayList agendaEvents = EventDatabase.getInstance(getContext()).GetRecords(1);
-
-//Updates the calendar view with relevant events
-        HashSet<Date> events = new HashSet<>();
-        MainCalendarView cv = ((MainCalendarView)root.findViewById(R.id.calendar_view));
-        cv.updateCalendar(events);
+        
         // creating new AgendaEventsAdapter (the custom view for the agenda events list) and applying that adapter to agendaListView
         AgendaEventsAdapter agendaEventsListAdapter = new AgendaEventsAdapter( getActivity(), agendaEvents);
         agendaListView.setAdapter(agendaEventsListAdapter);
