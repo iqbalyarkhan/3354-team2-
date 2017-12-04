c1f3383fd6cfcc73191cc3183b140dbd02a20dc3
Anmol Agarwal <aagarwal21@users.noreply.github.com>
2017-12-03 19:47:52 -0600

    Added test class for AddEvent method in EventDB

---
+++ src/CalendarApp/app/src/test/java/team2/calendarapp/AddEventToEventDBTest.java
@@ -0,0 +1,60 @@
+package team2.calendarapp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+/**
+ * Created by anmol on 12/3/2017.
+ */
+public class AddEventToEventDBTest {
+    private EventDB tester;
+    @Before
+    public void setUp() {
+        tester = EventDB.getInstance();
+    }
+
+    @Test
+    public void testAddEventNullDate(){
+        // set event start time and end time to 0000, 00, 00
+        // This would make the event start and End time January 1, 1970, the default Calendar time
+        Calendar calStart = new GregorianCalendar(0000, 00, 00);
+        Calendar calEnd = new GregorianCalendar(0000, 00, 00);
+        Event newEvent = new Event("test", null, null, calStart,
+                calEnd, 0);
+            assertTrue(tester.addEvent(newEvent));
+        }
+    @Test
+    public void testAddNullEvent() {
+       // nullEvent should return false, not be added to database
+        Event newEvent = null;
+        assertFalse(tester.addEvent(newEvent));
+    }
+    @Test
+    public void testAddEventWithNoDuration(){
+        Calendar calStart = new GregorianCalendar(2017,12,03);
+        Calendar calEnd = new GregorianCalendar(2017,12,03);
+        // creating an event that has the same start and end time.
+        // should return true because you can have an event that starts and ends on the same day
+        Event newEvent = new Event(null,null,null,calStart, calEnd,0);
+         assertTrue(tester.addEvent(newEvent));
+    }
+    @Test
+    public void testAddNormalEvent(){
+        // add a normal calendar event should return true
+        Calendar calStart = new GregorianCalendar(2017,12,03);
+        Calendar calEnd = new GregorianCalendar(2017,12,06);
+        Event newEvent = new Event("test android project", "use JUnit Framework","Home",
+                calStart, calEnd, 1);
+        assertTrue(tester.addEvent(newEvent));
+
+    }
+
+
+}
